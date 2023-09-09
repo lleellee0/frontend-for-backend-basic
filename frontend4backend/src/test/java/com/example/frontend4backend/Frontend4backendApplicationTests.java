84dc8f5b25b24cf01ad6b9466234ff51efa3bfb4
Foorogrammer <lleellee013@gmail.com>
2023-09-09 22:49:25 +0900

    Add files via upload

---
+++ frontend4backend/src/test/java/com/example/frontend4backend/Frontend4backendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.frontend4backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Frontend4backendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
